8f1116e2ff7aad1953f2d4835eeafda3b01fcf69
Sakthi7420 <Sakthivel.M@gwcdata.ai>
2024-09-13 16:03:04 +0530

    logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,6 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    screens: {
-      'tablet': '640px',
-      'laptop': '1024px',
-      'desktop': '1280px',
-    },
     extend: {},
     
   },
